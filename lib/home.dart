1a84aa6875264b9253ab871e990693f244be4df7
mirsadkt <mirsadkt@gmail.com>
2023-12-31 17:24:48 +0530

    Edited the Project a bit to start coding.

---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_constructors, unused_import
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    );
+  }
+}
